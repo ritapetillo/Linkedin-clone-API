a4c6965e23dd23f9f5bc0345329e91405c8ac3ca
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-01-26 07:24:45 -0500

    follow done

--- src/server.js
+++ src/server.js
@@ -5,8 +5,7 @@ const error_handler = require("node-error-handler");
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require("./services/index");
 const mongoose = require("mongoose");
-const listEndpoints = require("express-list-endpoints")
-
+const listEndpoints = require("express-list-endpoints");
 
 //MIDDLEWARES
 server.use(express.json());
@@ -17,10 +16,9 @@ server.use("/api", apiRoutes);
 
 //ERROR HANDLERS
 server.use(error_handler({ log: true, debug: true }));
-console.log(listEndpoints(server))
+// console.log(listEndpoints(server))
 
-
-console.log("Endpoints:", listEndpoints(apiRoutes))
+// console.log("Endpoints:", listEndpoints(apiRoutes))
 
 //Connect to DB and server
 mongoose
