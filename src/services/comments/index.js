1a3adb686771ca7c2a89579598b8ea3a16041705
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-26 21:18:31 +0100

    adds reply delete

--- src/services/comments/index.js
+++ src/services/comments/index.js
@@ -39,45 +39,46 @@ router.get("/:id", async (req, res, next) => {
 });
 
 router.put("/:id/user/:uid", async (req, res, next) => {
-  try{
-    const commentToUpdate = await CommentsModel.findById(req.params.id)
-  if(commentToUpdate.user[0] == req.params.uid)
-    {
+  try {
+    const commentToUpdate = await CommentsModel.findById(req.params.id);
+    if (commentToUpdate.user[0] == req.params.uid) {
       try {
-      const comment = await CommentsModel.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-          runValidators: true,
-          new: true,
+        const comment = await CommentsModel.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          {
+            runValidators: true,
+            new: true,
+          }
+        );
+        console.log("COMMENT:::::", comment.user[0]);
+        if (comment) {
+          res.send(comment);
+        } else {
+          const error = new Error(`comment with id ${req.params.id} not found`);
+          error.httpStatusCode = 404;
+          next(error);
         }
-      );
-      console.log("COMMENT:::::", comment.user[0]);
-      if (comment) {
-        res.send(comment);
-      } else {
-        const error = new Error(`comment with id ${req.params.id} not found`);
-        error.httpStatusCode = 404;
+      } catch (error) {
         next(error);
       }
-    } catch (error) {
+    } else {
+      const error = new Error(
+        "only the author of the comment can update his/her comment"
+      );
+      error.httpStatusCode = 403;
       next(error);
-    } 
-  } else {
-    const error = new Error('only the author of the comment can update his/her comment');
-    error.httpStatusCode = 403;
-    next(error);
+    }
+  } catch (error) {
+    console.log(error);
   }
-} catch (error){
-  console.log(error)
-}
 });
 
 router.delete("/:id/user/:uid", async (req, res, next) => {
   try {
     const commmentToDelete = await CommentsModel.findById(req.params.id);
-    if(commmentToDelete.user[0] == req.params.uid){
-      try{
+    if (commmentToDelete.user[0] == req.params.uid) {
+      try {
         const comment = await CommentsModel.findByIdAndDelete(req.params.id);
         if (comment) {
           res.send(req.params.id);
@@ -86,18 +87,22 @@ router.delete("/:id/user/:uid", async (req, res, next) => {
           error.httpStatusCode = 404;
           next(error);
         }
-      } catch (error){
+      } catch (error) {
         const er = new Error(`Something went wrong`);
-          er.httpStatusCode = 400;
-          next(er);
+        er.httpStatusCode = 400;
+        next(er);
       }
     } else {
-      const er = new Error(`only the author of the comment or the author can delete the comment`);
+      const er = new Error(
+        `only the author of the comment or the author can delete the comment`
+      );
       er.httpStatusCode = 403;
       next(er);
     }
   } catch (error) {
-    const e = new Error(`only the author of the comment or the author can delete the comment`);
+    const e = new Error(
+      `only the author of the comment or the author can delete the comment`
+    );
     e.httpStatusCode = 403;
     next(e);
   }
@@ -123,104 +128,129 @@ router.post(
 );
 
 // /comments/:id/replies GET
-router.get("/:id/replies", async(req, res, next) => {
-  try{
+router.get("/:id/replies", async (req, res, next) => {
+  try {
     const { replies } = await CommentsModel.findById(req.params.id);
     res.status(200).send(replies);
-  } catch(error){
+  } catch (error) {
     const err = new Error("Something went wrong with GET.");
     err.httpStatusCode = 400;
-    next(err)
+    next(err);
   }
-})
+});
 
 // /comments/:id/replies POST
-router.post("/:id/replies", async(req, res, next) => {
-  try{
+router.post("/:id/replies", async (req, res, next) => {
+  try {
     const replyText = req.body.text;
     const replyAuthorId = req.body.user;
-    
+
     const replyToInsert = {
       text: replyText,
-      user: replyAuthorId
-    }
+      user: replyAuthorId,
+    };
     console.log("REPLY TO INSERT:::::::", replyToInsert);
-    if(replyAuthorId){
-    const updatedComment = await CommentsModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $push: {
-          replies: replyToInsert,
+    if (replyAuthorId) {
+      const updatedComment = await CommentsModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $push: {
+            replies: replyToInsert,
+          },
+        },
+        {
+          runValidators: true,
+          new: true,
         }
-      },
-      {
-        runValidators: true,
-        new: true
-      }
-    );
+      );
     } else {
       throw new Error();
     }
     res.status(201).send(updatedComment);
-
-  } catch(error){
+  } catch (error) {
     const err = new Error("Something went wrong with POST.");
     err.httpStatusCode = 500;
-    next(err)
+    next(err);
   }
-})
+});
 
 // /comments/:cid/replies/:rid/user/:uid
-router.put("/:cid/replies/:rid/user/:uid", async(req, res, next) => {
-  try{
+router.put("/:cid/replies/:rid/user/:uid", async (req, res, next) => {
+  try {
     const { replies } = await CommentsModel.findById(req.params.cid, {
       _id: 0,
       replies: {
         $elemMatch: {
-          _id: req.params.rid
-        }
-      }
+          _id: req.params.rid,
+        },
+      },
     });
 
-    console.log("reply user id:::::::::::", replies[0].user[0])
+    console.log("reply user id:::::::::::", replies[0].user[0]);
 
-    if(replies && replies.length > 0 && req.params.uid == replies[0].user[0]){
-      const replyToUpdate = {...replies[0].toObject(), ...req.body}
+    if (replies && replies.length > 0 && req.params.uid == replies[0].user[0]) {
+      const replyToUpdate = { ...replies[0].toObject(), ...req.body };
       console.log("repy to update:::::::", replyToUpdate);
       const modifiedReply = await CommentsModel.findOneAndUpdate(
         {
           _id: mongoose.Types.ObjectId(req.params.cid),
-          "replies._id": mongoose.Types.ObjectId(req.params.rid)
+          "replies._id": mongoose.Types.ObjectId(req.params.rid),
         },
-        { $set: { "replies.$": replyToUpdate}}
-      )
+        { $set: { "replies.$": replyToUpdate } }
+      );
       console.log("modifiedReply:::::::", modifiedReply);
-      res.status(200).send(modifiedReply)
+      res.status(200).send(modifiedReply);
     } else {
       const error = new Error("Couldnt update reply with id=", req.params.rid);
-      next(error)
+      next(error);
     }
-  } catch(error){
+  } catch (error) {
     const err = new Error("hereeeeeeeeeeeeeeeeeee");
-    next(err)
+    next(err);
   }
-})
+});
 
 // /comments/:cid/replies/:rid/user/:uid
-router.delete("/:cid/replies/:rid/user/:uid", async(req, res, next) => {
-  try{
-    const modifiedReply = await CommentsModel.findByIdAndUpdate(
-      req.params.cid,
-      {
-        $pull: {
-          replies: {_id: req.params.rid}
-        }
-      }
+router.delete("/:cid/replies/:rid/user/:uid", async (req, res, next) => {
+  try {
+    const { replies } = await CommentsModel.findById(req.params.cid, {
+      _id: 0,
+      replies: {
+        $elemMatch: {
+          _id: req.params.rid,
+        },
+      },
+    });
+    console.log(
+      "REPLIES::::::::::::",
+      replies[0].user[0],
+      "USER:::::::",
+      req.params.uid
     );
-    res.status(200).send(modifiedReply)
-  } catch(error){
-    const err = new Error("Couldnt delete reply!");
-    next(err)
+
+    if (replies[0].user[0] == req.params.uid) {
+      try {
+        const modifiedReply = await CommentsModel.findByIdAndUpdate(
+          req.params.cid,
+          {
+            $pull: {
+              replies: { _id: req.params.rid },
+            },
+          }
+        );
+        res.status(200).send(modifiedReply);
+      } catch (error) {
+        const err = new Error("Couldnt delete reply!");
+        next(err);
+      }
+    } else {
+      const err = new Error(
+        "Only the author of the reply can delete the reply!"
+      );
+      next(err);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
